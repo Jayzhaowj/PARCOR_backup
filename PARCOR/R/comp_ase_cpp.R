dbe9ece7c5cfbcc92de83bc1346f1d0dd1d7e88e
Wenjie Zhao <wjzhao@ucsc.edu>
2020-10-24 00:58:31 -0700

    fix bugs in comp.ase.cpp

--- PARCOR/R/comp_ase_cpp.R
+++ PARCOR/R/comp_ase_cpp.R
@@ -5,7 +5,7 @@ comp.ase.cpp <- function(phi, SIGMA, phi_true, SIGMA_true, P_max, start = 0.001,
   w <- seq(start, end, by = interval)
   n_t <- dim(phi)[2]
   ase <- c(0, 0, 0)
-  TT <- length((P_max+1):(n_t-P_max))
+  TT <- (P_max+1):(n_t-P_max)
   true_sd <- compute_spec(phi = phi_true, SIGMA = SIGMA_true, w = w, P_max = P_max, ch1 = 1, ch2 = 2)
   est_sd <- compute_spec(phi = phi, SIGMA = SIGMA, w = w, P_max = P_max, ch1 = 1, ch2 = 2)
   ase[1] <- mean((est_sd[[1]][TT, ] - true_sd[[1]][TT, ])^2)
